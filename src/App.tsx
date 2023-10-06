d67bfb68ad83704030c67ccd10f26b07ca08d6b0
markusVJH <markus.heinila@pp.inet.fi>
2023-10-06 13:05:56 +0300

    store, usersSlice, hooks

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
         <Route path="/" element={<Layout />} />
         <Route path="/about" element={<About />} />
         <Route path="/users" element={<UsersView />} />
+        {/* <Route path="/users:userId" element={} /> */}
       </Routes>
       </BrowserRouter>
     </div>
